36d0db05f532869ca895c98a2ab3e1f5478ea1f8
Anton Molchanovskyi <mywear1@gmail.com>
2021-10-18 12:13:04 +0300

    auth_works

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,15 @@ const bcrypt = require( 'bcrypt' )
 const { User, Basket } = require( '../models/models' )
 const jwt = require( 'jsonwebtoken' )
 
+
+const generateJwt = ( id, email, role ) =>
+{
+    return jwt.sign(
+        { id, email, role },
+        process.env.SECRET_KEY,
+        { expiresIn: '24h' }
+    )
+}
 class UserController
 {
     async registration ( req, res, next )
@@ -20,35 +29,52 @@ class UserController
         const hashPassword = await bcrypt.hash( password, 5 )
         const user = await User.create( { email, role, password: hashPassword } )
         const basket = await Basket.create( { userId: user.id } )
-        const token = jwt.sign(
-            { id: user.id, email, role },
-            process.env.SECRET_KEY,
-            { expiresIn: '24h' }
-        )
-        return res.json( token )
+        // const token = jwt.sign(
+        //     { id: user.id, email, role },
+        //     process.env.SECRET_KEY,
+        //     { expiresIn: '24h' }
+        // )
+        const token = generateJwt( user.id, user.email, user.role )
 
+        return res.json( { token } )
 
     }
-    async login ( req, res )
+    async login ( req, res, next )
     {
-
+        const { email, password } = req.body
+        const user = await User.findOne( { where: { email } } )
+        if ( !user )
+        {
+            return next( ApiError.badRequest( 'Пользователь не найден' ) )
+        }
+        let comparePassword = bcrypt.compareSync( password, user.password )
+        if ( !comparePassword )
+        {
+            return next( ApiError.badRequest( 'Неверный пароль' ) )
+        }
+        const token = generateJwt( user.id, user.email, user.role )
+        return res.json({token})
     }
     async check ( req, res, next )
     {
-        const query = req.query
+        // const query = req.query
+        // const { id } = req.query
+        // console.log( "TCL: id", id )
+        // if ( !id )
+        // {
+        //     console.log( "не ид" )
+        //     return next( ApiError.badRequest( 'не задан id' ) )
+        // }
+        // res.json( id )
 
-        const { id } = req.query
-        console.log( "TCL: id", id )
+        // res.json({message:'all works'})
 
-        if ( !id )
-        {
-            console.log( "не ид" )
-            return next( ApiError.badRequest( 'не задан id' ) )
-        }
-        res.json( id )
+        const token = generateJwt(req.user.id,req.user.email,req.user.role)
+        return res.json({token})
+
     }
 
-
+//корзину и рейтинг
 }
 
 module.exports = new UserController()
