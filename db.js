587f426c8a48c25a4ad1f841a93944ea81c2bcf4
Anton Molchanovskyi <mywear1@gmail.com>
2021-11-15 13:05:41 +0200

    db_conn_string

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const { Sequelize } = require( 'sequelize' );
+
 process.env.NODE_ENV="production";
+
 DATABASE_URL='postgres://qgilefayctavim:5027d2cb19434db55904e0c1091723fd34c68839ec0957861fec416d3c2ce34f@ec2-3-222-11-129.compute-1.amazonaws.com:5432/d7jnu23kq4nunb'
 
 module.exports = new Sequelize( process.env.NODE_ENV === "production" ?
-    {
-        connectionString: process.env.DATABASE_URL
-    } :
+    
+       DATABASE_URL
+     :
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
