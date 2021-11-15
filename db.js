446721c6a4128db5094614e3fbefb0fd2a3614a8
Anton Molchanovskyi <mywear1@gmail.com>
2021-11-15 15:26:18 +0200

    env7

--- db.js
+++ db.js
@@ -1,21 +1,9 @@
 const { Sequelize } = require( 'sequelize' );
 
-// process.env.NODE_ENV="production";
+process.env.NODE_ENV="production";
 
 DATABASE_URL = 'postgres://qgilefayctavim:5027d2cb19434db55904e0c1091723fd34c68839ec0957861fec416d3c2ce34f@ec2-3-222-11-129.compute-1.amazonaws.com:5432/d7jnu23kq4nunb'
 
-// module.exports = new Sequelize(
-//     DATABASE_URL,
-//     {
-//         dialect: "postgres",
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//                 rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-//             }
-//         },
-//     }
-// )
 
 
 module.exports = new Sequelize( process.env.NODE_ENV === "production" ?
@@ -40,3 +28,16 @@ module.exports = new Sequelize( process.env.NODE_ENV === "production" ?
     }
 
 )
+
+// module.exports = new Sequelize(
+//     DATABASE_URL,
+//     {
+//         dialect: "postgres",
+//         dialectOptions: {
+//             ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+//             }
+//         },
+//     }
+// )
